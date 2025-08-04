f3b627eb2f4dd2cd3fd2f6a8473077eee17d51df
seoyoung33 <bsy4357@gmail.com>
2025-08-04 19:36:05 +0900

    닥터지 3행

--- scripts/index.js
+++ scripts/index.js
@@ -8,3 +8,17 @@ const mainAd = new Swiper ('#main_ad',{
         type:'bullets'
     }
 });
+const bestItems = new Swiper ('#best_items',{
+    loop:true,
+    direction:'horizontal',
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+    },
+    breakpoints:{
+        1250:{
+            slidesPerView:5,
+            spaceBetween:45,
+        },
+    }
+})

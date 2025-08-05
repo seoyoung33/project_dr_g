199081f4f64511522bfc2919476ef9aa37906711
폴리나 <elfish33@gmail.com>
2025-08-05 16:07:54 +0900

    저장

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,21 @@ const bestItems = new Swiper ('#best_items',{
     loop:true,
     direction:'horizontal',
     scrollbar: {
-        el: '.swiper-scrollbar',
+        el: '.scrollbar1',
+        draggable: true,
+    },
+    breakpoints:{
+        1250:{
+            slidesPerView:5,
+            spaceBetween:45,
+        },
+    }
+});
+const newItems = new Swiper ('#new_items',{
+    loop:true,
+    direction:'horizontal',
+    scrollbar: {
+        el: '.scrollbar2',
         draggable: true,
     },
     breakpoints:{

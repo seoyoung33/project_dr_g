fc7491e278f68a3c2a42077f8d4d1fc3c61c5116
seoyoung33 <bsy4357@gmail.com>
2025-08-05 19:39:03 +0900

    브랜드랑 푸터만 하면 된다

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,16 @@ const mainAd = new Swiper ('#main_ad',{
         type:'bullets'
     }
 });
-/*  */
 const bestItems = new Swiper ('#best_items',{
     loop:true,
     direction:'horizontal',
     scrollbar: {
         el: '.scrollbar1',
         draggable: true,
+    },
+    navigation:{
+        nextEl:'#best_items~.swiper-button-next',
+        prevEl:'#best_items~.swiper-button-prev',
     },
     breakpoints:{
         1250:{
@@ -30,10 +33,30 @@ const newItems = new Swiper ('#new_items',{
         el: '.scrollbar2',
         draggable: true,
     },
+    navigation:{
+        nextEl:'#new_items~.swiper-button-next',
+        prevEl:'#new_items~.swiper-button-prev',
+    },
     breakpoints:{
         1250:{
             slidesPerView:5,
             spaceBetween:45,
         },
     }
+});
+const videoAll = new Swiper ('#video_all',{
+    loop:true,
+    direction:'horizontal',
+    scrollbar: {
+        el: '.scrollbar3',
+        draggable: true,
+    },
+    navigation:{
+        nextEl:'#video_all~.swiper-button-next',
+        prevEl:'#video_all~.swiper-button-prev',
+    },
+});
+const bnr = new Swiper ('#ad_bnr_swiper',{
+    loop:true,
+    direction:'horizontal',
 })
